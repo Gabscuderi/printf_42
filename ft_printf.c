36d16e8d5b378fdc27d46aa3908556da1c24cb2c
Gabriele Scuderi <gscuderi@e3r6p8.42roma.it>
2024-02-26 20:31:21 +0100

    printf write on mandatory

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,48 @@
 /*   By: gscuderi <gscuderi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:05:03 by gscuderi          #+#    #+#             */
-/*   Updated: 2024/02/24 17:07:25 by gscuderi         ###   ########.fr       */
+/*   Updated: 2024/02/26 20:30:18 by gscuderi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "myprintf.h"
+/*#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>*/
+
+static int	ft_printf_format(char specifer);
 
 int	ft_printf(const char *format, ...)
 {
+	va_list variadic;
+	int		count;
+	int		i;
 
+	count = 0;
+	//i = 0;
+	va_start(variadic, format);
+	while (format[count])
+	{
+		if (format[count] == '%')
+			i += ft_printf_format(format[count + 1]);
+		write(1, &format[count], 1);
+		i++;
+		count++;
+	}
+	va_end(variadic);
+	return (i);
 }
+static int	ft_printf_format(char specifer)
+{
+	if (specifer == 'd' || specifer == 'i')
+		// putnbr
+}
+/*
+int main ()
+{
+	int i;
+	i = ft_printf("hola");
+	printf("%d", i);
+	return 0;
+}
+*/
--- myprintf.h
+++ myprintf.h
@@ -6,15 +6,17 @@
 /*   By: gscuderi <gscuderi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:07:59 by gscuderi          #+#    #+#             */
-/*   Updated: 2024/02/24 17:09:13 by gscuderi         ###   ########.fr       */
+/*   Updated: 2024/02/26 20:30:24 by gscuderi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//stdarg lib with va_list, and the 3 macros va_start va_arg e va_end
 
 #ifndef MYPRINTF_H
 # define MYPRINTF_H
 
 # include <unistd.h>
-# include 
+# include <stdarg.h>
 
 int	ft_printf(const char *format, ...);
 
